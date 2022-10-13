4eeed36382bcc09e5af0de052f1579bdf4897f65
TavaresDylan <tavares.dylan03@gmail.com>
2022-10-14 00:18:32 +0200

    feat: lint config

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,10 +7,10 @@ app.use(express.json());
 
 app.use(express.static("./"));
 
-app.use("/", function(req,res){
-    res.sendFile(path.join(__dirname+"/index.html"))
+app.use("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(port, ()=>{
-    console.log("server is running on "+port)
-})
+app.listen(port, () => {
+  console.log("server is running on " + port);
+});
